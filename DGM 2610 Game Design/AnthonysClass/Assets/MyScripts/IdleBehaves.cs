1f575ee3fb361a505f80860d854d487819398ef7
Unknown <caboosereynolds@gmail.com>
2018-03-01 15:43:52 -0700

    anthony homework further along

--- DGM 2610 Game Design/AnthonysClass/Assets/MyScripts/IdleBehaves.cs
+++ DGM 2610 Game Design/AnthonysClass/Assets/MyScripts/IdleBehaves.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//Using RoboRyantron.Unite2017.Events;
+using RoboRyanTron.Unite2017.Events;
 
 public class IdleBehaves : StateMachineBehaviour {
 
 public GameEvent Event;
 	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	Event.Raise();
+		Event.Raise();
 	}
 }
---
+++ DGM 2610 Game Design/AnthonysClass/Assets/MyScripts/OnStateExitEvent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RoboRyanTron.Unite2017.Events;
+
+public class OnStateExitEvent : StateMachineBehaviour {
+public GameEvent Event;
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	Event.Raise();
+	}
+}
